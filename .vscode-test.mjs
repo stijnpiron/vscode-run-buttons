a1334891d20d73ff2ec09cd66a8e667390eb50fc
Stijn Piron <stijn.piron@gmail.com>
2024-10-17 13:00:32 +0200

    update tests

---
+++ .vscode-test.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "@vscode/test-cli";
+
+export default defineConfig([
+  {
+    label: "unitTests",
+    files: "out/test/**/*.test.js",
+    workspaceFolder: "./sampleWorkspace",
+    mocha: {
+      ui: "tdd",
+      timeout: 20000,
+    },
+  },
+]);
