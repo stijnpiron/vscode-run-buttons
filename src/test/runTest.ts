3411f3d5e87538fa1aad00079015a7789605c7a3
Stijn Piron <stijn.piron@gmail.com>
2024-10-18 17:22:49 +0200

    add code coverage

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,10 +6,14 @@ async function main() {
     const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
     const extensionTestsPath = path.resolve(__dirname, "./suite");
 
+    console.log("Extension Development Path:", extensionDevelopmentPath);
+    console.log("Extension Tests Path:", extensionTestsPath);
+
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
       version: "insiders",
+      
     });
   } catch (err) {
     console.error("Failed to run tests", err);
